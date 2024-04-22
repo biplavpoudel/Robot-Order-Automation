3ab256bd0e294c947bd1ab5c5fbefdf85fbb1508
Biplav Poudel <biplavpoudel764@gmail.com>
2024-04-22 14:54:20 +0545

    Silly mistake. Forgot to add () to function

--- tasks.py
+++ tasks.py
@@ -16,11 +16,12 @@ def order_robots_from_RobotSpareBin():
     Creates ZIP archive of the receipts and the images.
     """
     browser.configure(
-        slowmo = 1000,
+        slowmo = 100,
     )
     open_robot_order_website()
     orders = get_order()
     fill_the_form(orders)
+    archive_receipts()
 
 def open_robot_order_website():
     """Connect to website"""
@@ -49,15 +50,13 @@ def fill_the_form(data):
         page.select_option('//*[@id="head"]', str(record['Head']))
         page.click('//*[@id="id-body-{}"]'.format(str(record['Body'])))
         page.fill("[placeholder='Enter the part number for the legs']", record['Legs'])
-        # page.fill("[id='1713712001487']", record['Legs'])
         page.fill('//*[@id="address"]', record['Address'])
         preview_robot(record)
         submit_order()
-        pdf_file = store_receipt_as_pdf(record['Order number'])
-        screenshot = screenshot_robot(record['Order number'])
+        pdf_file = store_receipt_as_pdf(str(record['Order number']))
+        screenshot = screenshot_robot(str(record['Order number']))
         embed_screenshot_to_receipt(screenshot, pdf_file)
         next_order()
-        archive
 
 def preview_robot(record):
     """Preview the order"""
@@ -71,39 +70,42 @@ def preview_robot(record):
 def submit_order():
     """Submit Order or Try again"""
     page = browser.page()
-    while (True):
-        try:
-            page.click('//*[@id="order"]')
-        except:
-            print("Order cannot be submitted! Trying again.")
-            continue
+
+    page.click("button:text('Order')")
+    while page.locator("//div[@class='alert alert-danger']").is_visible():
+        page.click("button:text('Order')")
+
 
 def store_receipt_as_pdf(order_number) -> str:
     """Receipt is saved as pdf"""
-    page = browser.page()
-    receipt_html = page.locator('#receipt').inner_html()
-    pdf = PDF()
-    path = f"output/receipts/receipt{order_number}.pdf"
-    pdf.html_to_pdf(receipt_html, path)
-    return str(path)
+    try:
+        page = browser.page()
+        receipt_html = page.locator('//*[@id="receipt"]').inner_html()
+        pdf = PDF()
+        path = f"output/receipts/receipt{order_number}.pdf"
+        pdf.html_to_pdf(receipt_html, path)
+        return str(path)
+    except Exception as e:
+        print(f"Error: {str(e)}")
     
 
 def screenshot_robot(order_number) -> str:
     """Screenshots the robot"""
     page = browser.page()
-    path = f'output/screenshots/screenshot{order_number}.png'
+    out_path = f'output/screenshots/screenshot{order_number}.png'
     image_loc = page.locator('//*[@id="robot-preview-image"]')
-    page.screenshot(image_loc, path = path)
-    return str(path)
+    image_loc.screenshot(path=out_path)
+    return str(out_path)
 
 def embed_screenshot_to_receipt(screenshot, pdf_file):
     pdf = PDF()
-    pdf.add_files_to_pdf(files = screenshot, target_document = pdf_file, append=True)
+    # pdf.add_files_to_pdf(files = str(screenshot), target_document = str(pdf_file), append=True)
+    pdf.add_watermark_image_to_pdf(screenshot, pdf_file, pdf_file)
 
 
 def next_order():
     page = browser.page()
-    page.click("button:text('ORDER ANOTHER ROBOT')")
+    page.click("button:text('Order another robot')")
 
 def archive_receipts():
     lib = Archive()
